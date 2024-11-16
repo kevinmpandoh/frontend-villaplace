d6163a9631c3bd07027e363f362125a7fd1fbebb
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2024-11-16 21:05:53 +0800

    feat: menambahkan middleware di nextjs

---
+++ src/middleware.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  if (request.cookies.has("tokenUser")) {
+    if (
+      request.nextUrl.pathname.startsWith("/auth/login")
+      //   request.nextUrl.pathname.startsWith("/auth/user/register")
+    ) {
+      return NextResponse.redirect(new URL("/", request.url));
+    } else {
+      return NextResponse.next();
+    }
+  } else {
+    if (
+      request.nextUrl.pathname.startsWith("/auth")
+      //   request.nextUrl.pathname.startsWith("/auth/user/register")
+    ) {
+      return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: [
+    "/profile/:path*",
+    // "/waiting-payment/:path*",
+    // "/order/:path*",
+    // "/review/:path*",
+    // "/profile/:path*",
+    "/auth/:path*",
+  ],
+};
