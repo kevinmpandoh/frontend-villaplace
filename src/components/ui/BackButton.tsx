7541fee6da9e28829e927efc62063597587dd852
Azkhal20 <azkalzavier@gmail.com>
2024-12-18 17:26:27 +0700

    feat: menambahkan modal pada manajemen posting

--- src/components/ui/BackButton.tsx
+++ src/components/ui/BackButton.tsx
@@ -5,13 +5,13 @@ import { faArrowLeft} from '@fortawesome/free-solid-svg-icons';
 const BackButton: React.FC = () => {
   return (
     <Link href="/" passHref>
-      <a 
+      <button 
         className="inline-flex items-center gap-2 justify-center px-3 py-2 rounded-lg hover:bg-green-100 border-2 border-primary text-primary font-semibold text-center"
         aria-label='Back'
       >
           <FontAwesomeIcon icon={faArrowLeft} data-testid="fa-arrow-left" />
           <span>Back</span>
-      </a>
+      </button>
     </Link>
   );
 };
